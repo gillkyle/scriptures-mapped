ed93258f5f17578eb2d219c2f6b675f5c8d280b8
gillkyle <kylerobertgill@gmail.com>
2019-01-23 18:25:28 -0700

    part 2

--- js/scriptures.js
+++ js/scriptures.js
@@ -3,9 +3,12 @@ const Scriptures = (function() {
   // CONSTANTS
 
   // PRIVATE VARS
+  let books;
+  let volumes;
 
   // PRIVATE METHOD DECLARATIONS
   let ajax;
+  let cacheBooks;
   let init;
 
   // PRIVATE METHODS
@@ -32,12 +35,43 @@ const Scriptures = (function() {
     request.send();
   };
 
+  cacheBooks = function(callback) {
+    volumes.forEach(volume => {
+      let volumeBooks = [];
+      let bookId = volume.minBookId;
+
+      while (bookId <= volumeBooks.maxBookId) {
+        volumeBooks.push(books[bookId]);
+        bookId += 1;
+      }
+
+      volume.books = volumeBooks;
+    });
+
+    if (typeof callback === "function") {
+      callback();
+    }
+  };
+
   init = function(callback) {
+    let booksLoaded = false;
+    let volumesLoaded = false;
+
     ajax("https://scriptures.byu.edu/mapscrip/model/books.php", data => {
-      console.log(data);
+      books = data;
+      booksLoaded = true;
+
+      if (volumesLoaded) {
+        cacheBooks(callback);
+      }
     });
     ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", data => {
-      console.log(data);
+      volumes = data;
+      volumesLoaded = true;
+
+      if (booksLoaded) {
+        cacheBooks(callback);
+      }
     });
   };
 
